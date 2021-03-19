c0f112096adca38d281dfbb99c004b5437c6c1de
Ercenk Keresteci <ercenk@ercenk.com>
2021-03-19 12:32:57 -0700

    remove dependency for client library source

--- src/Microsoft.Marketplace/generate.cmd
+++
@@ -1,7 +0,0 @@
-cd Microsoft.Marketplace.SaaS
-@REM dotnet build /t:GenerateCode -p:AutoRestInput="Microsoft.Marketplace.SaaS.md"
-dotnet build /t:GenerateCode 
-
-cd ..\Microsoft.Marketplace.Metering
-@REM dotnet build /t:GenerateCode -p:AutoRestInput="Microsoft.Marketplace.Metering.md"
-dotnet build /t:GenerateCode 
